28cb1fdad198f0b675bd3ec8b34b932f9286d33c
dualmoon <dual@gmx.com>
2014-03-09 21:41:11 +0000

    Further error checking in image rehoster.

--- routes/rehost.js
+++ routes/rehost.js
@@ -4,10 +4,15 @@ var request = require('request'),
 exports.getImg = function(req, res){
     var rqurl = req.url.split("?")[1];
     //later I will rewrite imgur image page urls to direct images
+    if(/((http:\/\/)|^)(www.)?imgur\.com\//.test(rqurl)){
+        var rest = rqurl.replace(/((http:\/\/)|^)(www.)?imgur\.com\//,"");
+        
+    }
     //maybe the same for twimg
     var rq = request(rqurl,function(e,r,body){
-        console.log("error: ",e);
-        if(!e && r.statusCode==200){
+        if(!e && 
+           r.statusCode==200 && 
+           r.headers['content-type'].indexOf('image') > -1){
             var b64 = fs.readFileSync('temp/temp.png').toString('base64');
             res.send(200,"data:image/png;base64,"+b64);
         } else {
