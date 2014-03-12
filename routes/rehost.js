d9857277df5720ba7082ecbc1f7d484c0243cfc5
dualmoon <dual@gmx.com>
2014-03-12 15:59:49 +0000

    Initial support for imgur html and /gallery/ links.

--- routes/rehost.js
+++ routes/rehost.js
@@ -1,22 +1,27 @@
 var request = require('request'),
     fs = require('fs');
-
-exports.getImg = function(req, res){
+exports.getImg = function(req, res) {
     var rqurl = req.url.split("?")[1];
+    var test = true;
     //later I will rewrite imgur image page urls to direct images
-    if(/((http:\/\/)|^)(www.)?imgur\.com\//.test(rqurl)){
-        var rest = rqurl.replace(/((http:\/\/)|^)(www.)?imgur\.com\//,"");
-        
-    }
-    //maybe the same for twimg
-    var rq = request(rqurl,function(e,r,body){
-        if(!e && 
-           r.statusCode==200 && 
-           r.headers['content-type'].indexOf('image') > -1){
+    if(/((http:\/\/)|^)(www.)?imgur\.com\//.test(rqurl)) {
+        var rest = rqurl.replace(/((http:\/\/)|^)(www.)?imgur\.com\//, "");
+        if(/gallery/.test(rest)){
+            rest=rest.replace(/gallery/,"");
+            rest=rest.replace(/\//,"");
+        } else if(/^a\//.test(rest)) {
+            test = false;
+        }
+        rqurl = 'http://i.imgur.com/'+rest+'.jpg';
+    } //maybe the same for twimg
+    console.log("DEBUG: about to request "+rqurl);
+    var rq = request(rqurl, function(e, r, body) {
+        var ctype = r.headers['content-type'];
+        if(!e && r.statusCode == 200 && ctype.indexOf('image') > -1 && test) {
             var b64 = fs.readFileSync('temp/temp.png').toString('base64');
-            res.send(200,"data:image/png;base64,"+b64);
+            res.send(200, "data:" + ctype + ";base64," + b64);
         } else {
-            res.send(404,"Could not get image.");
+            res.send(404, "Could not get image.");
         }
     }).pipe(fs.createWriteStream('temp/temp.png'));
 };
