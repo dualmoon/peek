020496cfef2ea0999e271e8f39982869a81d9bd8
dualmoon <dual@gmx.com>
2014-03-22 21:39:12 +0000

    Update the way rehost replies to send JSON data instead, allowing proper handling of width/height attributes and styles.

--- routes/rehost.js
+++ routes/rehost.js
@@ -1,5 +1,6 @@
 var request = require('request'),
-    fs = require('fs');
+    fs = require('fs'),
+    sizeOf = require('image-size');
 exports.getImg = function(req, res) {
     var rqurl = req.url.split("?")[1];
     var test = true;
@@ -18,11 +19,16 @@ exports.getImg = function(req, res) {
     console.log("DEBUG: about to request " + rqurl);
     var rq = request(rqurl, function(e, r, body) {
         var ctype = r.headers['content-type'];
+        var size = sizeOf('temp/temp.png');
         if(!e && r.statusCode == 200 && ctype.indexOf('image') > -1 && test) {
             var b64 = fs.readFileSync('temp/temp.png').toString('base64');
-            res.send(200, "data:" + ctype + ";base64," + b64);
+            res.json(200, { response: "Image retrieve successful.",
+                             datauri: "data:" + ctype + ";base64," + b64, 
+                               width: size.width,
+                              height: size.height
+                          });
         } else {
-            res.send(404, "Could not get image.");
+            res.json(404, { response:"Could not get image." });
         }
     }).pipe(fs.createWriteStream('temp/temp.png'));
 };
